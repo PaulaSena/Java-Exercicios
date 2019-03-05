29d41261b06d63e7736dd6c6e449eed0b29bed36
PaulaSena <senasilvaanapaula@gmail.com>
2019-03-05 17:48:44 -0300

    Entrada de dados
    Atribuindo entrada de dados Teclado. com nextLine e nextFloat

--- javaexercicios/TiposPrimitivos.java
+++ javaexercicios/TiposPrimitivos.java
@@ -1,19 +1,30 @@
 /*
- * 
- * System.out.printf("A nota é %.2f", nota);
+ * System.out.printf("A nota é %.2f", nota);  ou 
+ * System.out.format("A nota de %s é %.1f \n", nome , nota);
  * System.out.println("A nota é:" + nota);
  */
 package javaexercicios;
+import java.util.Scanner;
 
 /*   formatado  float %f
- *   utilizar e duas casa decimais  %.2f ou  para substituir o float 
+ *   utilizar e duas casa decimais  %.2f ou  para chamar o float %f ou String %s
  *   pular linha     \n 
+ *   atribuindo entrada de dados Teclado. com nextLine e nextFloat
  */
 public class TiposPrimitivos {
     public static void main(String[] args) {
-        float nota = 8.5f;
-        System.out.printf("A nota é %.2f \n", nota);
-        
+    	Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite o nome do Aluno:");
+       	String nome = teclado.nextLine();
+        System.out.println("Digite a nota do aluno:");
+        float nota = teclado.nextFloat();
+        System.out.printf("A nota de %s é %.1f \n", nome , nota);
+       /* if{
+           > 10 ;}
+        else{
+        }
+        */
+      
     }
     }
     
