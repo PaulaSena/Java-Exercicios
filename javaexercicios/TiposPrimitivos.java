640ecec877b10f31081b4d4ad48eea28f9b8a85e
PaulaSena <senasilvaanapaula@gmail.com>
2019-03-05 17:08:05 -0300

    Create TiposPrimitivos.java

---
+++ javaexercicios/TiposPrimitivos.java
@@ -0,0 +1,21 @@
+/*
+ * 
+ * System.out.printf("A nota é %.2f", nota);
+ * System.out.println("A nota é:" + nota);
+ */
+package javaexercicios;
+
+/*   formatado  float %f
+ *   utilizar e duas casa decimais  %.2f ou  para substituir o float 
+ *   pular linha     \n 
+ */
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        float nota = 8.5f;
+        System.out.printf("A nota é %.2f \n", nota);
+        
+    }
+    }
+    
+    
+
